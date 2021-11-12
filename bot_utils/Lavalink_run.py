e29060aa0bbccb06660983deaf5c22c8a76ce2ba
KortaPo <70193867+KortaPo@users.noreply.github.com>
2021-11-13 01:29:51 +0530

    Added logging to everything.

--- bot_utils/Lavalink_run.py
+++ bot_utils/Lavalink_run.py
@@ -11,20 +11,22 @@ def run_lavalink_process():
     """A function that runs the Lavalink server"""
     process = subprocess.run(['which', program], capture_output=True, text=True)
     if process.returncode == 0:
-        print(
-            f'The program "{program}" is installed. Lavalink can run.')  # checking if Java is installed on the host 
-        # system. 
-        print(f'The location of the binary is: {process.stdout}')
+        logger.success(
+            f'The program "{program}" is installed. Lavalink can be run.')  # checking if Java is installed on the host
+        # system.
+        logger.info(f'The location of the binary is: {process.stdout}')
 
         output = subprocess.run(bash_command, shell=True, capture_output=True, text=True)  # running the command on
         # shell.
-        # and yes this command is dangerous.
-        logger.info("Lavalink has started.")
-        print(output.stdout)
-        logger.info("Lavalink is running.")
+        # and yes using shell kwarg can be dangerous.
+        if output.returncode == 0:
+            logger.success("Lavalink is running.")
+        else:
+            logger.error(f"Lavalink is not running. Error: {output.stderr}")
     else:
-        print(f'Sorry program {program} is not installed in your system. Please install it in order to run our Jar '
-              f'application.')
+        logger.error(f'Sorry program {program} is not installed in your system. Please install it in order to run our '
+                     f'Jar '
+                     f'application.')
         print(process.stderr)
         logger.info("Lavalink is not running.")
         return
