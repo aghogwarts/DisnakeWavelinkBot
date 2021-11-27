d7056e0efad6ab6fef37979e535cde2acb94e839
KortaPo <70193867+KortaPo@users.noreply.github.com>
2021-11-27 18:26:32 +0530

    Exceptions for filters

--- wavelink/eqs.py
+++ wavelink/eqs.py
@@ -20,176 +20,49 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-import collections
-
-from wavelink.errors import FilterInvalidArgument
 
 
-class Equalizer:
+class WavelinkException(Exception):
     """
-    Class representing a usable equalizer.
-
-    Parameters
-    ------------
-    levels: List[Tuple[int, float]]
-        A list of tuple pairs containing a band int and gain float.
-    name: str
-        An Optional string to name this Equalizer. Defaults to 'CustomEqualizer'
-
-    Attributes
-    ------------
-    eq: list
-        A list of {'band': int, 'gain': float}
-    raw: list
-        A list of tuple pairs containing a band int and gain float.
+    Base Wavelink Exception.
     """
 
-    def __init__(self, *, levels: list, name: str = "CustomEqualizer"):
-        self.eq = self._factory(levels)
-        self.raw = levels
 
-        self._name = name
+class NodeOccupied(WavelinkException):
+    """
+    Exception raised when node identifiers conflict.
+    """
 
-    def __str__(self):
-        return self._name
 
-    def __repr__(self):
-        return f"<wavelink.eqs.Equalizer: {self._name}, Raw: {self.eq}>"
+class InvalidIDProvided(WavelinkException):
+    """
+    Exception raised when an invalid ID is passed somewhere in Wavelink.
+    """
 
-    @property
-    def name(self):
-        """
-        The Equalizers friendly name.
-        """
-        return self._name
 
-    @staticmethod
-    def _factory(levels: list):
-        _dict = collections.defaultdict(int)
+class ZeroConnectedNodes(WavelinkException):
+    """
+    Exception raised when an operation is attempted with nodes, when there are None connected.
+    """
 
-        _dict.update(levels)
-        _dict = [{"band": i, "gain": _dict[i]} for i in range(15)]
 
-        return _dict
+class AuthorizationFailure(WavelinkException):
+    """Exception raised when an invalid password is provided toa node."""
 
-    @classmethod
-    def build(cls, *, levels: list, name: str = "CustomEqualizer"):
-        """
-        Build a custom Equalizer class with the provided levels.
 
-        Parameters
-        ------------
-        levels: List[Tuple[int, float]]
-            A list of tuple pairs containing a band int and gain float.
-        name: str
-            An Optional string to name this Equalizer. Defaults to 'CustomEqualizer'
-        """
-        return cls(levels=levels, name=name)
+class BuildTrackError(WavelinkException):
+    """
+    Exception raised when a track is failed to be decoded and re-built.
+    """
 
-    @classmethod
-    def flat(cls):
-        """
-        Flat Equalizer.
 
-        Resets your EQ to Flat.
-        """
-        levels = [
-            (0, 0.0),
-            (1, 0.0),
-            (2, 0.0),
-            (3, 0.0),
-            (4, 0.0),
-            (5, 0.0),
-            (6, 0.0),
-            (7, 0.0),
-            (8, 0.0),
-            (9, 0.0),
-            (10, 0.0),
-            (11, 0.0),
-            (12, 0.0),
-            (13, 0.0),
-            (14, 0.0),
-        ]
+class FilterInvalidArgument(WavelinkException):
+    """
+    Raised when an invalid argument is passed to a filter.
+    """
 
-        return cls(levels=levels, name="Flat")
 
-    @classmethod
-    def boost(cls):
-        """
-        Boost Equalizer.
-
-        This equalizer emphasizes Punchy Bass and Crisp Mid-High tones.
-        Not suitable for tracks with Deep/Low Bass.
-        """
-        levels = [
-            (0, -0.075),
-            (1, 0.125),
-            (2, 0.125),
-            (3, 0.1),
-            (4, 0.1),
-            (5, 0.05),
-            (6, 0.075),
-            (7, 0.0),
-            (8, 0.0),
-            (9, 0.0),
-            (10, 0.0),
-            (11, 0.0),
-            (12, 0.125),
-            (13, 0.15),
-            (14, 0.05),
-        ]
-
-        return cls(levels=levels, name="Boost")
-
-    @classmethod
-    def metal(cls):
-        """
-        Experimental Metal/Rock Equalizer.
-
-        Expect clipping on Bassy songs.
-        """
-        levels = [
-            (0, 0.0),
-            (1, 0.1),
-            (2, 0.1),
-            (3, 0.15),
-            (4, 0.13),
-            (5, 0.1),
-            (6, 0.0),
-            (7, 0.125),
-            (8, 0.175),
-            (9, 0.175),
-            (10, 0.125),
-            (11, 0.125),
-            (12, 0.1),
-            (13, 0.075),
-            (14, 0.0),
-        ]
-
-        return cls(levels=levels, name="Metal")
-
-    @classmethod
-    def piano(cls):
-        """Piano Equalizer.
-
-        Suitable for Piano tracks, or tacks with an emphasis on Female Vocals.
-        Could also be used as a Bass Cutoff.
-        """
-        levels = [
-            (0, -0.25),
-            (1, -0.25),
-            (2, -0.125),
-            (3, 0.0),
-            (4, 0.25),
-            (5, 0.25),
-            (6, 0.0),
-            (7, -0.25),
-            (8, -0.25),
-            (9, 0.0),
-            (10, 0.0),
-            (11, 0.5),
-            (12, 0.25),
-            (13, -0.025),
-        ]
-
-        return cls(levels=levels, name="piano")
+class InvalidFilter(WavelinkException):
+    """
+    Raised when an invalid filter is passed to a filter.
+    """
