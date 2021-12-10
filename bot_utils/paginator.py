658b78d1034ec6c3ebe62cf19f56cdb38da1aafd
KortaPo <70193867+KortaPo@users.noreply.github.com>
2021-12-11 02:39:30 +0530

    Added a new and improved ViewPaginator

--- bot_utils/paginator.py
+++ bot_utils/paginator.py
@@ -6,12 +6,298 @@ import typing
 from typing import List
 
 import disnake
+
+from bot_utils import menus
 from bot_utils.menus import ListPageSource
+
+
+class ViewPages(disnake.ui.View):
+    """
+    A view paginator that can be used to display a list of items in embeds as pages.
+    """
+
+    def __init__(
+        self,
+        source: menus.PageSource,
+        *,
+        ctx: disnake.ApplicationCommandInteraction,
+        check_embeds: bool = True,
+        compact: bool = False,
+    ):
+        super().__init__()
+        self.source: menus.PageSource = source
+        self.check_embeds: bool = check_embeds
+        self.ctx: disnake.ApplicationCommandInteraction = ctx
+        self.message: typing.Optional[disnake.Message] = None
+        self.current_page: int = 0
+        self.compact: bool = compact
+        self.input_lock = asyncio.Lock()
+        self.clear_items()
+        self.fill_items()
+
+    def fill_items(self) -> None:
+        """
+        Fills the items with the items from the source.
+        """
+        if not self.compact:
+            self.numbered_page.row = 1
+            self.stop_pages.row = 1
+
+        if self.source.is_paginating():
+            max_pages = self.source.get_max_pages()
+            use_last_and_first = max_pages is not None and max_pages >= 2
+            if use_last_and_first:
+                self.add_item(self.go_to_first_page)
+            self.add_item(self.go_to_previous_page)
+            if not self.compact:
+                self.add_item(self.go_to_current_page)
+            self.add_item(self.go_to_next_page)
+            if use_last_and_first:
+                self.add_item(self.go_to_last_page)
+            if not self.compact:
+                self.add_item(self.numbered_page)
+            self.add_item(self.stop_pages)
+
+    async def _get_kwargs_from_page(self, page: int) -> typing.Dict[str, typing.Any]:
+        """
+        Gets the keyword arguments from a page (embed).
+
+        Parameters
+        ----------
+        page : int
+            The page (embed) to get the keyword arguments from.
+        """
+        value = await disnake.utils.maybe_coroutine(self.source.format_page, self, page)
+        if isinstance(value, dict):
+            return value
+        elif isinstance(value, str):
+            return {"content": value, "embed": None}
+        elif isinstance(value, disnake.Embed):
+            return {"embed": value, "content": None}
+        else:
+            return {}
+
+    async def show_page(
+        self, interaction: disnake.Interaction, page_number: int
+    ) -> None:
+        """
+        Shows the page with the given number.
+
+
+        Parameters
+        ----------
+        interaction : disnake.Interaction
+            The interaction that triggered this page.
+
+        page_number : int
+            The page number to show.
+
+        """
+        page = await self.source.get_page(page_number)
+        self.current_page = page_number
+        kwargs = await self._get_kwargs_from_page(page)
+        self._update_labels(page_number)
+        if kwargs:
+            if interaction.response.is_done():
+                if self.message:
+                    await self.message.edit(**kwargs, view=self)
+            else:
+                await interaction.response.edit_message(**kwargs, view=self)
+
+    def _update_labels(self, page_number: int) -> None:
+        """
+        Updates the labels of the numbered page button.
+
+        Parameters
+        ----------
+        page_number : int
+            The page number to update the label of.
+        """
+        self.go_to_first_page.disabled = page_number == 0
+        if self.compact:
+            max_pages = self.source.get_max_pages()
+            self.go_to_last_page.disabled = (
+                max_pages is None or (page_number + 1) >= max_pages
+            )
+            self.go_to_next_page.disabled = (
+                max_pages is not None and (page_number + 1) >= max_pages
+            )
+            self.go_to_previous_page.disabled = page_number == 0
+            return
+
+        self.go_to_current_page.label = str(page_number + 1)
+        self.go_to_previous_page.label = str(page_number)
+        self.go_to_next_page.label = str(page_number + 2)
+        self.go_to_next_page.disabled = False
+        self.go_to_previous_page.disabled = False
+        self.go_to_first_page.disabled = False
+
+        max_pages = self.source.get_max_pages()
+        if max_pages is not None:
+            self.go_to_last_page.disabled = (page_number + 1) >= max_pages
+            if (page_number + 1) >= max_pages:
+                self.go_to_next_page.disabled = True
+                self.go_to_next_page.label = "…"
+            if page_number == 0:
+                self.go_to_previous_page.disabled = True
+                self.go_to_previous_page.label = "…"
+
+    async def show_checked_page(
+        self, interaction: disnake.Interaction, page_number: int
+    ) -> None:
+        max_pages = self.source.get_max_pages()
+        try:
+            if max_pages is None:
+                # If it doesn't give maximum pages, it cannot be checked
+                await self.show_page(interaction, page_number)
+            elif max_pages > page_number >= 0:
+                await self.show_page(interaction, page_number)
+        except IndexError:
+            # If an Index Error happens, it has been handled, so it can be ignored.
+            pass
+
+    async def interaction_check(self, interaction: disnake.Interaction) -> bool:
+        if interaction.user and interaction.user.id in (
+            self.ctx.bot.owner_id,
+            self.ctx.author.id,
+        ):
+            return True
+        await interaction.response.send_message(
+            "This pagination menu cannot be controlled by you, sorry!", ephemeral=True
+        )
+        return False
+
+    async def on_timeout(self) -> None:
+        if self.message:
+            await self.message.edit(view=None)
+
+    async def on_error(
+        self, error: Exception, item: disnake.ui.Item, interaction: disnake.Interaction
+    ) -> None:
+        if interaction.response.is_done():
+            await interaction.followup.send(
+                "An unknown error occurred, sorry", ephemeral=True
+            )
+        else:
+            await interaction.response.send_message(
+                "An unknown error occurred, sorry", ephemeral=True
+            )
+
+    async def start(self) -> None:
+        if (
+            self.check_embeds
+            and not self.ctx.channel.permissions_for(self.ctx.me).embed_links
+        ):
+            await self.ctx.send(
+                "Bot does not have embed links permission in this channel."
+            )
+            return
+
+        await self.source._prepare_once()
+        page = await self.source.get_page(0)
+        kwargs = await self._get_kwargs_from_page(page)
+        self._update_labels(0)
+        self.message = await self.ctx.send(**kwargs, view=self)
+
+    @disnake.ui.button(label="≪", style=disnake.ButtonStyle.grey)
+    async def go_to_first_page(
+        self, button: disnake.ui.Button, interaction: disnake.Interaction
+    ):
+        """
+        A button to go to the first page.
+        """
+        await self.show_page(interaction, 0)
+
+    @disnake.ui.button(label="Back", style=disnake.ButtonStyle.blurple)
+    async def go_to_previous_page(
+        self, button: disnake.ui.Button, interaction: disnake.Interaction
+    ):
+        """
+        A button to go to the previous page.
+        """
+        await self.show_checked_page(interaction, self.current_page - 1)
+
+    @disnake.ui.button(label="Current", style=disnake.ButtonStyle.grey, disabled=True)
+    async def go_to_current_page(
+        self, button: disnake.ui.Button, interaction: disnake.Interaction
+    ):
+        pass
+
+    @disnake.ui.button(label="Next", style=disnake.ButtonStyle.blurple)
+    async def go_to_next_page(
+        self, button: disnake.ui.Button, interaction: disnake.Interaction
+    ):
+        """
+        A button that goes to the next page.
+
+        Parameters
+        ----------
+        button : disnake.ui.Button
+            The button that was pressed.
+
+        interaction : disnake.Interaction
+            The interaction that was performed.
+
+        """
+        await self.show_checked_page(interaction, self.current_page + 1)
+
+    @disnake.ui.button(label="≫", style=disnake.ButtonStyle.grey)
+    async def go_to_last_page(
+        self, button: disnake.ui.Button, interaction: disnake.Interaction
+    ):
+        """
+        A button to go to the last page.
+
+        Parameters
+        ----------
+        button : disnake.ui.Button
+            The button that was pressed.
+
+        interaction : disnake.Interaction
+            The interaction that was performed.
+
+        Returns
+        -------
+        None
+        """
+        # The call here is safe because it's guarded by skip_if
+        await self.show_page(interaction, self.source.get_max_pages() - 1)
+
+    @disnake.ui.button(label="Quit", style=disnake.ButtonStyle.red)
+    async def stop_pages(
+        self, button: disnake.ui.Button, interaction: disnake.Interaction
+    ):
+        """stops the pagination session."""
+        await interaction.response.defer()
+        await interaction.delete_original_message()
+        self.stop()
+
+
+class RichPager(menus.ListPageSource):
+    """
+    A class that formats the page (embed) to add more information.
+
+    """
+
+    async def format_page(self, menu, entries):
+        pages = []
+        for index, entry in enumerate(entries, start=menu.current_page * self.per_page):
+            pages.append(f"{index + 1}. {entry}")
+
+        maximum = self.get_max_pages()
+        if maximum > 1:
+            footer = (
+                f"Page {menu.current_page + 1}/{maximum} ({len(self.entries)} entries)"
+            )
+            menu.embed.set_footer(text=footer)
+
+        menu.embed.description = "\n".join(pages)
+        return menu.embed
 
 
 class EmbedPaginator(disnake.ui.View):
     """
-    A paginator that displays a list of items in an embed.
+    A paginator that displays a list of embeds.
     """
 
     def __init__(
@@ -256,7 +542,7 @@ class EmbedPaginator(disnake.ui.View):
         """
         embed = self.embeds[0]
         embed.set_footer(text=f"Page 1/{len(self.embeds)}")
-        self.message = await self.ctx.channel.send(embed=embed, view=self)
+        self.message = await self.ctx.edit_original_message(embed=embed, view=self)
 
 
 class SimpleEmbedPages(EmbedPaginator):
