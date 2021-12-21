cc3e5c7440f53da29e24f53b1350853fa671c84f
KortaPo <70193867+KortaPo@users.noreply.github.com>
2021-12-21 20:43:37 +0530

    Fixes, minor changes,

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
-import yaml
+#  -*- coding: utf-8 -*-
+
+
 from loguru import logger
 from MusicBot import Bot
 from bot_utils.Lavalink_run import lavalink_alive
 import time
-from yaml import load
-
-with open("config.yaml") as f:
-    config = load(f, Loader=yaml.Loader)
+from bot_utils.Helpers_ import Config
 
 if __name__ == "__main__":
 
     bot = Bot()
+    config = Config()
     bot.load_cogs("cogs")
     logger.info("All cogs have been successfully loaded", __name="Music Bot")
-    lavalink_alive()
+    lavalink_alive()  # running Lavalink. It's important to run this before the bot starts.
     try:
         time.sleep(14)  # Wait for lavalink to start
 
-        bot.run(config["token"][0], reconnect=True)
+        bot.run(config.token, reconnect=True)
     except KeyboardInterrupt:
         try:
             bot.session.close()  # Closing the ClientSession
@@ -29,10 +29,10 @@ if __name__ == "__main__":
 
     except SystemExit:
         try:
-            bot.session.close()  # Closing the ClientSession
             logger.info("Closing Aiohttp ClientSession...", __name="Music Bot")
-            bot.clear()  # clearing the cache
+            bot.session.close()  # Closing the ClientSession
             logger.info("Clearing Bot Cache", __name="Music Bot")
+            bot.clear()  # clearing the cache
             exit(0)
         except AttributeError:
             logger.info("Gracefully stopping the program....", __name="Music Bot")
