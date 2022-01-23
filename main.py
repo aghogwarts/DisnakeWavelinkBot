6c7121c173efa9142cc551198240c5024f87ce01
KortaPo <70193867+KortaPo@users.noreply.github.com>
2022-01-23 23:17:36 +0530

    Small changes

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 #  -*- coding: utf-8 -*-
+import sys
 import time
 
 from loguru import logger
 
 from core.MusicBot import Bot
 from utils.helpers import Config
-from utils.Lavalink_run import lavalink_alive
+from utils.process import lavalink_alive
 
 if __name__ == "__main__":
 
@@ -16,23 +17,23 @@ if __name__ == "__main__":
     logger.info("Starting Lavalink.....", __name="Music Bot")
     lavalink_alive()  # running Lavalink. It's important to run this before the bot starts.
     try:
-        time.sleep(14)  # Wait for lavalink to start
+        time.sleep(30)  # Wait for lavalink to start
 
         bot.run(config.token, reconnect=True)
     except KeyboardInterrupt:
         try:
             bot.session.close()  # Closing the ClientSession
             logger.info("Shutting down...", __name="Music Bot")
-            exit(0)
+            sys.exit(0)
         except AttributeError:
             logger.info("Exiting Program gracefully.", __name="Music Bot")
-            exit(0)
+            sys.exit(0)
 
     except SystemExit:
         try:
             logger.info("Clearing Bot Cache", __name="Music Bot")
             bot.clear()  # clearing the cache
-            exit(0)
+            sys.exit(0)
         except AttributeError:
             logger.info("Gracefully stopping the program....", __name="Music Bot")
             exit(0)
