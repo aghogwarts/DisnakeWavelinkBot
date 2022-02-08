126933f356a3a6bae23c188141fe6425fc7b7667
KortaPo <70193867+KortaPo@users.noreply.github.com>
2022-02-08 22:13:31 +0530

    Added uvloop support

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 #  -*- coding: utf-8 -*-
+import asyncio
 import sys
 import time
 
+import uvloop
 from loguru import logger
-
+import sys
 from core.MusicBot import Bot
 from utils.helpers import Config
-from utils.process import lavalink_alive
 
 if __name__ == "__main__":
-
+    if sys.platform == "Linux":
+        asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+    else:
+        pass
     bot = Bot()
     config = Config()
     bot.load_cogs("cogs")
     logger.info("All cogs have been successfully loaded", __name="Music Bot")
-    logger.info("Starting Lavalink.....", __name="Music Bot")
-    lavalink_alive()  # running Lavalink. It's important to run this before the bot starts.
+    logger.info("Starting Bot.....", __name="Music Bot")
     try:
-        time.sleep(30)  # Wait for lavalink to start
+        time.sleep(16)  # Wait for lavalink to start
 
         bot.run(config.token, reconnect=True)
     except KeyboardInterrupt:
